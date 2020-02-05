5474aa1c21cda08d78a713b3c6b58c5942d832be
Mikael Escolin <mikaelescolin@gmail.com>
2020-02-05 22:51:19 +0000

    Added a compile and run shell file

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -20,7 +20,7 @@ public class HelloProcessing extends PApplet
 		background(0, 255, 0);
 		
         stroke(0, 0, 255);
-		line(10, 10, 100, 100);	// x1, y1, x2, y2
+        line(10, 10, 100, 100);	// x1, y1, x2, y2
 		point(50, 50); // x, y
 		fill(0, 255, 255);
 		noStroke();
@@ -28,6 +28,5 @@ public class HelloProcessing extends PApplet
 		fill(255, 0, 0);
 		rect(70, 150, 90, 10); // tlx, tly, w, h
         triangle(150, 90, 200, 100, 100, 250);
-		
 	}
 }
