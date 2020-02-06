50b96dd77b571750a81983084929275573340938
Mikael Escolin <mikaelescolin@gmail.com>
2020-02-06 15:48:56 +0000

    Added functions to draw triangles using percentages according to screen size

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -21,7 +21,7 @@ public class HelloProcessing extends PApplet
 	public void draw()
 	{
         background(237, 25, 9);
-        drawTriangle(66.66, 20, 33.33, 80, 99.99, 80);
+        drawTriangle((float)40, 20, (float)20, 80, (float)90, 80);
 		/*
         stroke(0, 0, 255);
         line(10, 10, 100, 100);	// x1, y1, x2, y2
@@ -36,10 +36,22 @@ public class HelloProcessing extends PApplet
 
     }// END draw()
     
-    float getPercentageCoord(int percentage, int num)
+    private float getPercentageCoord(float percentage, float num)
     {
-        return (num / 100) * percentage;
+        return (float) (num / 100) * percentage;
     }// END getPercentageCoord()
 
-    float drawTriangle(float x1, float y1, float x2, float y2, float x3, float y3)
+    private void drawTriangle(float x1, float y1, float x2, float y2, float x3, float y3)
+    {
+        float tx1, ty1, tx2, ty2, tx3, ty3;
+    
+        tx1 = getPercentageCoord(x1, this.width);
+        ty1 = getPercentageCoord(y1, this.length);
+        tx2 = getPercentageCoord(x2, this.width);
+        ty2 = getPercentageCoord(y2, this.length);
+        tx3 = getPercentageCoord(x3, this.width);
+        ty3 = getPercentageCoord(y3, this.length);
+
+        triangle(tx1, ty1, tx2, ty2, tx3, ty3);
+    }// END drawTriangle()
 }// END CLASS HelloProcessing
